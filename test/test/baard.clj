509c90da2e6b9d7e18249d5f7cb24ae80988e55d
misfo <tedwardo2@gmail.com>
2011-12-27 23:19:40 -0600

    Use ring.mock

--- test/test/baard.clj
+++ test/test/baard.clj
@@ -1,6 +1,7 @@
 (ns test.baard
-  (:use [baard])
-  (:use [clojure.test]))
+  (:use baard
+        clojure.test
+        ring.mock.request))
 
 (defn response
   [body]
@@ -11,13 +12,13 @@
 
 (deftest request-method-matching
   (let [f (app [_ "hi"] (constantly hello))]
-    (is (= hello (f {:request-method :get, :uri "/hi"})))
-    (is (= hello (f {:request-method :post, :uri "/hi"}))))
+    (is (= hello (f (request :get "/hi"))))
+    (is (= hello (f (request :post "/hi")))))
   (let [f (app [:get "hi"] (constantly hello)
                :else (constantly not-found))]
-    (is (= hello (f {:request-method :get, :uri "/hi"})))
-    (is (= not-found (f {:request-method :post, :uri "/hi"})))))
+    (is (= hello (f (request :get "/hi"))))
+    (is (= not-found (f (request :post "/hi"))))))
 
 (deftest uri-decoding
   (let [f (app [_ "sp ace" "sl/ash"] (constantly hello))]
-    (is (= hello (f {:request-method :get, :uri "/sp%20ace/sl%2Fash"})))))
+    (is (= hello (f (request :get "/sp%20ace/sl%2Fash"))))))
